ed914be689b5edf5d2bff6f1595c60d24a7462ea
Светлана Каленюк <svetlanakaleniuk48@gmail.com>
2025-12-27 19:39:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-al
+const links = document.querySelectorAll(".pizza__link");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // убираем прыжок наверх
+
+    const page = link.dataset.page;
+    window.location.href = page;
+  });
+});
+const button = document.getElementById("bannerBtn");
+
+button.addEventListener("click", function () {
+  window.location.href = "details.html";
+});
